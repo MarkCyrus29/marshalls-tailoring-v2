c805869237928d7e9dad436a800ffa5f4875a976
Cyrus <markcyrus.serrano@gmail.com>
2026-04-01 13:46:34 +0800

    updated canon url to www.marshallstailoring.com

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://marshallstailoring.vercel.app/sitemap.xml",
+    sitemap: "www.marshallstailoring.com/sitemap.xml",
   };
 }
